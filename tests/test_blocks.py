aa809ee7187fa3543c8f05d6ca08f22541c29d85
Thibaud Colas <thibaudcolas@gmail.com>
2018-03-01 15:09:05 +0200

    test(engine): add missing tests

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -1,1 +1,66 @@
 from __future__ import absolute_import, unicode_literals
+
+import unittest
+
+from draftjs_exporter.dom import DOM
+from draftjs_exporter_markdown.blocks import code_block, list_wrapper, ol, prefixed_block, ul
+
+
+class TestBlocks(unittest.TestCase):
+    def test_prefixed_block(self):
+        self.assertEqual(DOM.render(prefixed_block('> ')({
+            'children': 'test'
+        })), '> test\n\n')
+
+    def test_code_block(self):
+        self.assertEqual(DOM.render(code_block({
+            'block': {
+                'data': {},
+            },
+            'children': 'test',
+        })), '```\ntest\n```\n\n')
+
+    def test_code_block_language(self):
+        self.assertEqual(DOM.render(code_block({
+            'block': {
+                'data': {
+                    'language': 'css',
+                },
+            },
+            'children': 'test',
+        })), '```css\ntest\n```\n\n')
+
+    def test_ul(self):
+        self.assertEqual(DOM.render(ul({
+            'block': {
+                'depth': 0,
+            },
+            'children': 'test',
+        })), '* test\n')
+
+    def test_ul_depth(self):
+        self.assertEqual(DOM.render(ul({
+            'block': {
+                'depth': 2,
+            },
+            'children': 'test',
+        })), '    * test\n')
+
+    def test_ol(self):
+        self.assertEqual(DOM.render(ol({
+            'block': {
+                'depth': 0,
+            },
+            'children': 'test',
+        })), '1. test\n')
+
+    def test_ol_depth(self):
+        self.assertEqual(DOM.render(ol({
+            'block': {
+                'depth': 2,
+            },
+            'children': 'test',
+        })), '    1. test\n')
+
+    def test_list_wrapper(self):
+        self.assertEqual(DOM.render(list_wrapper({})), '')
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,1 +1,60 @@
 from __future__ import absolute_import, unicode_literals
+
+import unittest
+
+from draftjs_exporter_markdown.engine import DOMMarkwdown
+
+
+class TestDOMMarkwdown(unittest.TestCase):
+    def test_create_tag(self):
+        self.assertEqual(DOMMarkwdown.render_debug(DOMMarkwdown.create_tag(
+            'p', {'class': 'intro'})), '<p class="intro"></p>')
+
+    def test_create_tag_empty(self):
+        self.assertEqual(DOMMarkwdown.render_debug(
+            DOMMarkwdown.create_tag('p')), '<p></p>')
+
+    def test_parse_html(self):
+        self.assertEqual(DOMMarkwdown.render(DOMMarkwdown.parse_html(
+            '<p><span>Test text</span></p>')), '<p><span>Test text</span></p>')
+
+    def test_append_child(self):
+        parent = DOMMarkwdown.create_tag('p')
+        DOMMarkwdown.append_child(parent, DOMMarkwdown.create_tag('span', {}))
+        self.assertEqual(DOMMarkwdown.render_debug(
+            parent), '<p><span></span></p>')
+
+    def test_append_child_identical_text(self):
+        parent = DOMMarkwdown.create_tag('p')
+        DOMMarkwdown.append_child(parent, 'test')
+        DOMMarkwdown.append_child(parent, 'test')
+        self.assertEqual(DOMMarkwdown.render_debug(parent), '<p>testtest</p>')
+
+    def test_append_child_identical_elements(self):
+        parent = DOMMarkwdown.create_tag('p')
+        DOMMarkwdown.append_child(parent, DOMMarkwdown.create_tag('br'))
+        DOMMarkwdown.append_child(parent, DOMMarkwdown.create_tag('br'))
+        self.assertEqual(DOMMarkwdown.render_debug(
+            parent), '<p><br/><br/></p>')
+
+    def test_render_attrs(self):
+        self.assertEqual(DOMMarkwdown.render_attrs({
+            'src': 'src.png',
+            'alt': 'img\'s alt',
+            'class': 'intro',
+        }), ' alt="img&#x27;s alt" class="intro" src="src.png"')
+
+    def test_render_children(self):
+        self.assertEqual(DOMMarkwdown.render_children([
+            'render children',
+            DOMMarkwdown.create_tag('p', {'class': 'intro'}),
+            'test test',
+        ]), 'render children<p class="intro"></p>test test')
+
+    def test_render(self):
+        self.assertEqual(DOMMarkwdown.render_debug(DOMMarkwdown.create_tag(
+            'p', {'class': 'intro'})), '<p class="intro"></p>')
+
+    def test_render_debug(self):
+        self.assertEqual(DOMMarkwdown.render_debug(DOMMarkwdown.create_tag(
+            'p', {'class': 'intro'})), '<p class="intro"></p>')
